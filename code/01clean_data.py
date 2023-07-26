41894793fe392d947fcf3eeca4fba199985c0fc8
Abhay Jolly <abhayjolly18@gmail.com>
2023-07-25 19:37:22 -0700

    Filtered columns and developer data in north america

--- code/01clean_data.py
+++ code/01clean_data.py
@@ -3,17 +3,27 @@ import constants as cs
 import zipfile
 
 
+
 def load_data() -> pd.DataFrame:
     with zipfile.ZipFile(f"../{cs.DATA_PATH}") as data_zip:
-   
         with data_zip.open(cs.FILENAME) as developer_data:
             return pd.read_csv(developer_data)
 
 def main():
     stackoverflow_data = load_data()
-    print(stackoverflow_data.head(5))
+    # print(stackoverflow_data.head(5))
+    stackoverflow_data = stackoverflow_data[cs.KEEP_COLUMNS]
+    # print(stackoverflow_data['Country'].unique())
 
-
+    # Filtering developers data in north america
+    stackoverflow_data = stackoverflow_data[(
+        (stackoverflow_data[cs.MAIN_BRANCH] == cs.DEVELOPERS) & 
+        ((stackoverflow_data[cs.COUNTRY] == cs.CANADA) | (stackoverflow_data[cs.COUNTRY] == cs.USA))
+        )]
+    
+    print(stackoverflow_data.shape)
+    
+    
 
 if __name__ == "__main__":
     main()
--- code/constants.py
+++ code/constants.py
@@ -1,2 +1,18 @@
 DATA_PATH: str = "data_compressed.zip"
 FILENAME: str = "survey_results_public.csv"
+
+# Keep Columns
+KEEP_COLUMNS: list = [
+    'MainBranch', 'Age', 'Employment', 'RemoteWork', 'EdLevel', 'LearnCodeCoursesCert', 'YearsCode', 'YearsCodePro', 'DevType', 'OrgSize', 'Country','Currency', 
+    'CompTotal', 'LanguageHaveWorkedWith','DatabaseHaveWorkedWith','PlatformHaveWorkedWith','WebframeHaveWorkedWith', 'MiscTechHaveWorkedWith','ToolsTechHaveWorkedWith',
+    'OfficeStackAsyncHaveWorkedWith', 'AISent','AISelect', 'AIToolCurrently Using','WorkExp', 'ProfessionalTech', 'Industry'
+                      ]
+# Columns
+MAIN_BRANCH: str = 'MainBranch'
+COUNTRY: str = 'Country'
+
+
+# Data
+DEVELOPERS: str = 'I am a developer by profession'
+USA: str = 'United States of America'
+CANADA: str = 'Canada'
